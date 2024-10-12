6fb3a1c0e5e8b07d8f439c74707ca5abe47ed66c
rohit uppalge <rohituppalge@gmail.com>
2024-10-12 23:19:42 +0530

    Day 6 and Day 7 added

---
+++ Day 6/p2.java
@@ -0,0 +1,17 @@
+class demo{
+	public static void main(String[]args){
+		int a=85;
+		if(a>=90 && a<=100){
+			System.out.println('A');
+		}else if(a>=80 && a<=90){
+			System.out.println('B');
+		}else if(a>=70 && a<=80){
+			System.out.println('C');
+		}else if(a>=60 && a<=70){
+			System.out.println('D');
+		}else{
+			System.out.println('E');
+		}
+	}
+}
+//output=B
